b0ebfc17e93bb958a2d8e664ccff3a8dac770e28
Miyajima Takaki <takaki.mz@gmail.com>
2021-07-19 13:12:33 +0900

    feat: can show Banner and items

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Row } from './components/Row'
+import { Banner } from './components/Banner'
+import { requests } from "./request";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
